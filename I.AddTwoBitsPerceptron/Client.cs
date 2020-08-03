1a470c3bd6d49254fdd6a2639ee23d5ad908e039
HrMilev <hr.milev@yahoo.bg>
2020-08-03 23:37:48 +0300

    init

---
+++ I.AddTwoBitsPerceptron/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace I.AddTwoBitsPerceptron
+{
+    public class Client
+    {
+        static void Main()
+        {
+            int byte1 = 0;
+            int byte2 = 0;
+
+            Perceptron NAND1 = new Perceptron(-3, -3, 4);
+            Perceptron NAND2 = new Perceptron(-3, -3, 4);
+            Perceptron NAND3 = new Perceptron(-3, -3, 4);
+            Perceptron NAND4 = new Perceptron(-3, -3, 4);
+            Perceptron NAND5 = new Perceptron(-3, -3, 4);
+
+            NAND1.Byte1 = byte1;
+            NAND1.Byte2 = byte2;
+
+            NAND2.Byte1 = byte1;
+            NAND2.Link2 = NAND1;
+
+            NAND3.Link1 = NAND1;
+            NAND3.Byte2 = byte2;
+
+            NAND4.Link1 = NAND2;
+            NAND4.Link2 = NAND3;
+
+            NAND5.Link1 = NAND1;
+            NAND5.Link2 = NAND1;
+
+            int zeroBit = NAND4.Result();
+            int firstBit = NAND5.Result();
+
+            Console.WriteLine($"{firstBit}{zeroBit}");
+        }
+    }
+}
---
+++ I.AddTwoBitsPerceptron/Perceptron.cs
@@ -0,0 +1,28 @@
+﻿namespace I.AddTwoBitsPerceptron
+{
+    public class Perceptron
+    {
+        private readonly int weigth1;
+        private readonly int weigth2;
+        private readonly int bias;
+
+        public Perceptron(int weigth1, int weigth2, int bias)
+        {
+            this.weigth1 = weigth1;
+            this.weigth2 = weigth2;
+            this.bias = bias;
+        }
+
+        public Perceptron Link1 { get; set; }
+        public Perceptron Link2 { get; set; }
+        public int Byte1 { get; set; }
+        public int Byte2 { get; set; }
+
+        public int Result()
+        {
+            return ((Link1 == null ? Byte1 : Link1.Result()) * weigth1
+                + (Link2 == null ? Byte2 : Link2.Result()) * weigth2
+                + bias) > 0 ? 1 : 0;
+        }
+    }
+}
